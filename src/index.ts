8559b71134c68285f14a9ec81387bdb5ebf6da61
TheAM01 <onevisionofficial55@gmail.com>
2026-05-25 22:28:54 +0500

    Added uploads :)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { connectDB } from "./config/database.ts";
 import "dotenv/config";
 import type { Request, Response } from "express";
 import cors from "cors";
+import multer from "multer";
 
 // importing middleware
 import { loggerMiddleware } from "./middleware/logger.ts";
@@ -18,6 +19,31 @@ import { authMiddleware } from "./middleware/auth.middleware.ts";
 const app = express();
 const PORT = 5000;
 const dir = path.resolve();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        const unique = `${Date.now()}-${Math.floor(Math.random() * 1e9)}`;
+        cb(null, `${unique}-${path.extname(file.originalname)}`)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowed = ['image/jpeg', 'image/png', 'image/webp', 'image/gif'];
+    if (allowed.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only image files are allowed"), false);
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024}
+})
+
 
 // connecting to database
 connectDB();
@@ -39,6 +65,19 @@ app.get("/", authMiddleware, (req: Request, res: Response) => {
     return res.json("Hello this is a protected route!")
 });
 
+app.post("/upload", upload.single("data-image"), (req: Request, res: Response) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "no file uploaded" });
+    }
+
+    res.json({
+        message: "Upload successful",
+        filename: req.file.filename,
+        originalName: req.file.originalname,
+        size: req.file.size,
+    });
+})
+
 // utilizing router
 app.use("/auth", authRouter);
 app.use("/api/batches", batchRouter);
@@ -57,3 +96,8 @@ app.listen(PORT, () => {
 // GET /api/batches/:batchId ->  get batchId
 // PATCH /api/batches/:batchId ->  edit
 // DELETE /api/batches/:batchId ->  DELETE
+
+
+// mongod -> local version of mongodb (server)
+// mongosh -> cli client to communicate with the server
+// compass -> GUI client 
